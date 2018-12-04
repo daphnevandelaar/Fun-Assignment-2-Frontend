fde2eb6315a7e7026b0e2f7817334138c0551755
Daphne <daphnevandelaar@live.nl>
2018-12-04 08:37:35 +0100

    datepicker added
    Next time create clickable list items for invoice

--- src/components/InvoiceCreator.js
+++ src/components/InvoiceCreator.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import {CustomerOverview} from './CustomerOverview/CustomerOverview.js'
+import DatePicker from "react-datepicker";
+ 
+import "react-datepicker/dist/react-datepicker.css";
+ 
 
 export default class InvoiceCreator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
   render() {
     return (
       <div>
@@ -23,9 +39,32 @@ export default class InvoiceCreator extends Component {
                   <li>Dienst 1</li>
                 </ul>
               </div>
-            </div>
-
           </div>
+          <div class="col-sm">
+            <h2>Klanten tab</h2>
+            <input type="text" placeholder="Klant zoeken"></input>
+              <div>
+                <ul>
+                  <li>Klant 1</li>
+                  <li>Klant 1</li>
+                  <li>Klant 1</li>
+                  <li>Klant 1</li>
+                  <li>Klant 1</li>
+                  <li>Klant 1</li>
+                </ul>
+              </div>
+          </div>
+          <div class="col-sm">
+            <h2>Factuur tab</h2>
+            <DatePicker
+              todayButton={"Vandaag"}
+              selected={this.state.startDate}
+              onChange={this.handleChange}
+              dateFormat="dd/MM/yyyy"
+
+            />
+          </div>
+        </div>
         </div>
       </div>
     )
